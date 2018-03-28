c149280405004a63f58ced9f0eeedc8cc512d058
Angelddg <angeldominguezg@gmail.com>
2018-03-28 12:39:50 -0400

    Saving styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <b-container class="bv-example-row bv-example-row-flex-cols">
     <vue-particles
         color="#dedede"
         :particleOpacity="0.7"
@@ -17,10 +17,48 @@
         :clickEffect="true"
         clickMode="push"
       >
-      </vue-particles>
-  </div>
+    </vue-particles>
+    <b-row>
+      <b-col align-self="center" class="main-title">
+        <div class="title-box">
+          <h1>Angel Dominguez</h1>
+          <h4>Subtitle here!</h4>
+          <hr>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 
 </script>
+
+<style>
+
+.main-title {
+  position: fixed;
+  top: 40%;
+  padding-left: 50px;
+  padding-right: 100px; 
+}
+
+#particles-js{
+  width: 110%;
+  height: 100%;
+  position: fixed;
+  right: 0;
+  background-color: #fff;
+  background-image: url('');
+  background-size: cover;
+  background-position: 50% 50%;
+  background-repeat: no-repeat;
+}
+
+.body-particles{
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 100;
+}
+</style>
